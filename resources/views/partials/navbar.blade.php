69edb6b70474ddcb295683711719217670a7005d
rahmaliabaani <rahmaliarpla15@gmail.com>
2022-12-09 15:14:29 +0700

    Buat Beranda

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Saget</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="/">Beranda</a>
+        <a class="nav-link" href="/resensi">Buku</a>
+        <a class="nav-link" href="/info">Info</a>
+        <a class="nav-link" href="/masuk">Masuk</a>
+      </div>
+    </div>
+  </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
+});
+
+Route::get('/info', function () {
+    return view('info');
 });
