79cff80a24801f80f378059547ae253765382a7d
Ayrus16 <suryap.sp83@gmail.com>
2022-12-28 08:44:07 +0700

    Upload Auth (Login, register, forgot pass)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('gambar');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
